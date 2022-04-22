4d995a5eff414edf4d84e3e65e8b7a0271f24e22
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 01:18:44 +0700

    feat: add validation for order, :order_date cannot be empty/falsy

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+  validates :order_date, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    describe '#order_date' do
+      it { should validate_presence_of(:order_date) }
+    end
+  end
 end

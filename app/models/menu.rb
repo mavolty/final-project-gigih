5a42a511e8366956fe65d9d710210a4809e6a9b7
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 19:53:17 +0700

    feat: add validation for menu, :price looks like a number greater than or equal to 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100, minimum: 3 }, uniqueness: true
   validates :description, length: { maximum: 150 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Menu, type: :model do
 
     describe '#price' do
       it { should validate_presence_of(:price) }
+      it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
     end
   end
 end

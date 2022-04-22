695fc110ca94da3becaccb28d7be234eec0ecfac
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 03:12:29 +0700

    feat: add test for GET /api/v1/orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class OrdersController < ApplicationController
       skip_before_action :verify_authenticity_token
+
+      def index
+        @orders = Order.all
+        render json: @orders, status: :ok
+      end
     end
   end
 end
--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Orders", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Orders', type: :request do
+  let!(:orders) { create_list(:order, 10) }
+  let(:order_id) { orders.first.id }
+
+  describe 'GET /api/v1/orders' do
+    before { get '/api/v1/orders' }
+
+    it 'returns orders' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end

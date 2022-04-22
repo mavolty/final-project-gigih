b10f9ca18d805571bf006ee8aba8383a6e569860
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 23:02:14 +0700

    feat: add test for categories_controller, this is literally same like menus_controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,38 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
+      def index
+        @categories = Category.all
+        render json: @categories, status: :ok
+      end
+
+      def show
+        @category = Category.find(params[:id])
+        render json: @category, status: :ok
+      end
+
+      def create
+        @category = Category.create!(category_params)
+        render json: @category, status: :created
+      end
+
+      def update
+        @category = Category.find(params[:id])
+        @category.update(category_params)
+        head :no_content
+      end
+
+      def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        head :no_content
+      end
+
+      private
+
+      def category_params
+        params.permit(:name)
+      end
     end
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,99 @@
 require 'rails_helper'
 
-RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Categories', type: :request do
+  let!(:categories) { create_list(:category, 10) }
+  let(:category_id) { categories.first.id }
+
+  describe 'GET /api/v1/categories' do
+    before { get '/api/v1/categories' }
+
+    it 'returns categories' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /api/v1/categories/:id' do
+    before { get "/api/v1/categories/#{category_id}" }
+
+    context 'when the record exists' do
+      it 'returns the category' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(category_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:category_id) { 'invalid-id' }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Category/)
+      end
+    end
+  end
+
+  describe 'POST /api/v1/categories' do
+    let(:valid_attributes) { { name: 'My Category' } }
+
+    context 'when the request is valid' do
+      before { post '/api/v1/categories', params: valid_attributes }
+
+      it 'creates a category' do
+        expect(json['name']).to eq('My Category')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/api/v1/categories', params: { name: nil } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed/)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/categories/:id' do
+    let(:valid_attributes) { { name: 'Foobar' } }
+
+    context 'when the record exists' do
+      before { put "/api/v1/categories/#{category_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/categories/:id' do
+    before { delete "/api/v1/categories/#{category_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
   end
 end

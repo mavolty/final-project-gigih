22727f22a15f9b66b18c08c7ad1cb7340e9bc955
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 03:09:21 +0700

    refactor: change quantity in factory order to customer_id

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order do
     order_date { Date.today }
     status { 'new' }
-    quantity { 1 }
-    customer { build(:customer) }
+    customer { create(:customer) }
+    customer_id { customer.id }
   end
 end

09344699f1fde9a6301200ca86845368e3396274
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 00:02:02 +0700

    feat: add test for GET /api/v1/menu_categories

--- app/controllers/api/v1/menu_categories_controller.rb
+++ app/controllers/api/v1/menu_categories_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class MenuCategoriesController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
+      def index
+        @menu_categories = MenuCategory.all
+        render json: @menu_categories
+      end
     end
   end
 end
--- spec/requests/menu_categories_spec.rb
+++ spec/requests/menu_categories_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "MenuCategories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:menu_category) { create(:menu_category) }
+  let(:menu_id) { menu_category.menu.id }
+  let(:category_id) { menu_category.category.id }
+
+  describe 'GET /api/v1/menu_categories' do
+    before { get '/api/v1/menu_categories' }
+
+    it 'returns menu_categories' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
-end
+ end

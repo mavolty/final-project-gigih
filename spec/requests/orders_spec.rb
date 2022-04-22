4b145b18cb93441db3c4d847339b946a72cedc7b
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 03:15:29 +0700

    feat: add test for GET /api/v1/orders/:id, when the record does not exist

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -30,5 +30,17 @@ RSpec.describe 'Orders', type: :request do
         expect(response).to have_http_status(200)
       end
     end
+
+    context 'when the record does not exist' do
+      let(:order_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Order/)
+      end
+    end
   end
 end

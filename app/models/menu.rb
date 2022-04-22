b0878277806148f727c05dc3af40accbd0e684e9
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 19:45:54 +0700

    feat: add validation for menu, the length of :name is at least 3

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,3 @@
 class Menu < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 100 }
+  validates :name, presence: true, length: { maximum: 100, minimum: 3 }
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Menu, type: :model do
     describe '#name' do
       it { should validate_presence_of(:name) }
       it { should validate_length_of(:name).is_at_most(100) }
+      it { should validate_length_of(:name).is_at_least(3) }
     end
   end
 end

9fb93409705286a8f9e9ed3f602ddc3bf369608d
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 21:31:38 +0700

    feat: add routes for menus api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :menus, only: %i[index show create update destroy]
+    end
+  end
 end

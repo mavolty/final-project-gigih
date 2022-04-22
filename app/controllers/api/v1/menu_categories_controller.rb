c6df299b97af63c46cb7ea7fd5d7b633cfecb7fa
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 00:03:47 +0700

    feat: add test for GET /api/v1/menu_categories/:menu_id, when the record exists

--- app/controllers/api/v1/menu_categories_controller.rb
+++ app/controllers/api/v1/menu_categories_controller.rb
@@ -7,6 +7,11 @@ module Api
         @menu_categories = MenuCategory.all
         render json: @menu_categories
       end
+
+      def show
+        @menu_category = Menu.find(params[:menu_id]).categories
+        render json: @menu_category, status: :ok
+      end
     end
   end
 end
--- spec/requests/menu_categories_spec.rb
+++ spec/requests/menu_categories_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe "MenuCategories", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /api/v1/menu_categories/:menu_id' do
+    before { get "/api/v1/menu_categories/#{menu_id}" }
+
+    context 'when the record exists' do
+      it 'returns the menu_category' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(menu_category.id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
  end

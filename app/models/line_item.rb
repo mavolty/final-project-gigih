815469fde999273a2d8b553c4aa9b590522bcf20
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 03:08:14 +0700

    feat: add total_price to line_item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,4 +5,8 @@ class LineItem < ApplicationRecord
   validates :menu_id, presence: true
   validates :order_id, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true }
+
+  def total_price
+    menu.price * quantity
+  end
 end

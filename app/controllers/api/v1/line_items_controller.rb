dd4e22522b48129ba756bc3f61979d5dde1fe7cd
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 03:21:04 +0700

    feat: add test for GET /api/v1/line_items

--- app/controllers/api/v1/line_items_controller.rb
+++ app/controllers/api/v1/line_items_controller.rb
@@ -2,6 +2,23 @@ module Api
   module V1
     class LineItemsController < ApplicationController
       skip_before_action :verify_authenticity_token
+
+      def index
+        @line_items = LineItem.all
+        @line_items = @line_items.map do |line_item|
+          {
+            id: line_item.id,
+            order_id: line_item.order_id,
+            menu_id: line_item.menu_id,
+            quantity: line_item.quantity,
+            total_price: line_item.total_price,
+            created_at: line_item.created_at,
+            updated_at: line_item.updated_at
+          }
+        end
+
+        render json: @line_items, status: :ok
+      end
     end
   end
 end
--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "LineItems", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'LineItems', type: :request do
+  let!(:line_items) { create_list(:line_item, 10) }
+  let(:line_item_id) { line_items.first.id }
+
+  describe 'GET /api/v1/line_items' do
+    before { get '/api/v1/line_items' }
+
+    it 'returns line_items' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end

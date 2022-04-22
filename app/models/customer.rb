a3a00209ca7bd1c3296f2a731aed9bf11f4a5a08
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 01:14:43 +0700

    feat: add validation for customer, not to allow :email to be ‹invalid_email›

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,3 @@
 class Customer < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Customer, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
   end
 end

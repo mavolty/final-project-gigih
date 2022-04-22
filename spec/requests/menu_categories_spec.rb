704b172e7fd1b4b7f0cf18a4b021a6520bf6087c
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 00:04:36 +0700

    feat: add test for GET /api/v1/menu_categories/:menu_id, when the record doest not exists

--- spec/requests/menu_categories_spec.rb
+++ spec/requests/menu_categories_spec.rb
@@ -31,5 +31,17 @@ RSpec.describe "MenuCategories", type: :request do
         expect(response).to have_http_status(200)
       end
     end
+
+    context 'when the record does not exist' do
+      let(:menu_id) { 'invalid-id' }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find MenuCategory/)
+      end
+    end
   end
  end

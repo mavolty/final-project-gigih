b9b155b0939025e5e26c006909dae1c1267f6152
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 20:24:42 +0700

    feat: add association for menu_category, belong to category required: true

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe MenuCategory, type: :model do
   describe 'associations' do
     it { should belong_to(:menu) }
+    it { should belong_to(:category) }
   end
 end

ba7c2aa1529981aa4c52ccfc6bc1ae09b13dac0a
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 19:41:11 +0700

    fix: add additional config for shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,9 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.

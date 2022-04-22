137e07ac08a5ea010e423ec7de1f309e15c7070c
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 01:23:52 +0700

    feat: add validation for order, :status is either ‹new›, ‹paid›, or ‹cancelled›

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ApplicationRecord
+  NEW = 'new'.freeze
+  PAID = 'paid'.freeze
+  CANCELLED = 'cancelled'.freeze
+
   belongs_to :customer
 
   validates :order_date, presence: true
   validates :customer_id, presence: true
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: [NEW, PAID, CANCELLED] }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Order, type: :model do
 
     describe '#status' do
       it { should validate_presence_of(:status) }
+      it { should validate_inclusion_of(:status).in_array(%w(new paid cancelled)) }
     end
   end
 end

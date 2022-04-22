4e8d0f852ebeb33a921928d4a29715c3dfb81102
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 20:02:13 +0700

    feat: add validation for category, the length of :name is at most 50

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Category, type: :model do
   describe 'validations' do
     describe '#name' do
       it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_most(50) }
     end
   end
 end

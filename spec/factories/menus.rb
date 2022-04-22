1be732ac95190d770ac924d32d8ef8f0992f30ed
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 21:20:13 +0700

    refactor: change name in menus factory to be unique

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :menu do
-    name { Faker::Food.dish[2..99] }
+    name { Faker::Name.unique.name[2..99] }
     description { Faker::Food.description[2..149] }
     price { 10_000.0 }
   end

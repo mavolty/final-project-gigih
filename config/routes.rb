45f37c28c3e3e5dcbbc7c93fad3849d93c6503f1
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 22:59:03 +0700

    feat: add routes for categories api

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :menus, only: %i[index show create update destroy]
+      resources :categories, only: %i[index show create update destroy]
     end
   end
 end

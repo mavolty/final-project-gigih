c22f22c4579502cd4ee3c81677230c82c1170072
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 20:12:29 +0700

    feat: add association for category, have many menu_categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  has_many :menu_categories
+
   validates :name, presence: true, length: { maximum: 50, minimum: 3 }, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Category, type: :model do
       it { should validate_uniqueness_of(:name) }
     end
   end
+
+  describe 'associations' do
+    it { should have_many(:menu_categories) }
+  end
 end

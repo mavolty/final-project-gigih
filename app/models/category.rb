80cfbae81291628f2ee27b2c9dd193d10688a677
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 20:04:22 +0700

    feat: add validation for category, :name is case-sensitively unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :name, presence: true, length: { maximum: 50, minimum: 3 }, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Category, type: :model do
       it { should validate_presence_of(:name) }
       it { should validate_length_of(:name).is_at_most(50) }
       it { should validate_length_of(:name).is_at_least(3) }
+      it { should validate_uniqueness_of(:name) }
     end
   end
 end

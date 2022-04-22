dcd76122e0aee191a5ee35240d57edccf7ec15a2
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 19:56:07 +0700

    feat: add validation for menu, have many menu_categories

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
 class Menu < ApplicationRecord
+  has_many :menu_categories
+
   validates :name, presence: true, length: { maximum: 100, minimum: 3 }, uniqueness: true
   validates :description, length: { maximum: 150 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe Menu, type: :model do
       it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
     end
   end
+
+  describe 'associations' do
+    it { should have_many(:menu_categories) }
+  end
 end

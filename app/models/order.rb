872502e15dde2083cd106b17005faea6b16e6ff0
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 02:22:04 +0700

    feat: set default value for :status and :order_date

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,21 @@ class Order < ApplicationRecord
   CANCELLED = 'cancelled'.freeze
 
   has_many :line_items, dependent: :destroy
-  has_many :menus , through: :line_items
+  has_many :menus, through: :line_items
   belongs_to :customer
 
   validates :order_date, presence: true
   validates :customer_id, presence: true
   validates :status, presence: true, inclusion: { in: [NEW, PAID, CANCELLED] }
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    return unless new_record?
+
+    self.order_date ||= Date.today
+    self.status ||= NEW
+  end
 end

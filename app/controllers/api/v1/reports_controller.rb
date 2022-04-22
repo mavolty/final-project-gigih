ae294e2c706b6e6b70778eeba1ec54ca1ab469ce
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-23 04:38:53 +0700

    feat: add test for GET /api/v1/reports

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2,6 +2,28 @@ module Api
   module V1
     class ReportsController < ApplicationController
       skip_before_action :verify_authenticity_token
+
+      def index
+        @orders = Order.all
+        @line_items = LineItem.all
+        @customers = Customer.all
+        @menus = Menu.all
+
+        @report = []
+        @line_items.each do |line_item|
+          if line_item.order.order_date.to_date == Date.today
+            @report << {
+              email: @customers.find(line_item.order.customer_id).email,
+              menu: @menus.find(line_item.menu_id).name,
+              quantity: line_item.quantity,
+              total_price: @menus.find(line_item.menu_id).price * line_item.quantity,
+              order_date: line_item.order.order_date
+            }
+          end
+        end
+
+        render json: @report, status: :ok
+      end
     end
   end
 end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Reports", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Reports', type: :request do
+  describe 'GET /api/v1/reports' do
+    before { get '/api/v1/reports' }
+
+    it 'returns reports' do
+      expect(json).not_to be_empty
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end

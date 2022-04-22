e752c72f38c19b1c2ef94b7d1f79f0af0ca86c2b
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-04-22 19:44:37 +0700

    feat: add validation for menu, the length of :name is at most 100

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Menu, type: :model do
   describe 'validations' do
     describe '#name' do
       it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_most(100) }
     end
   end
 end
